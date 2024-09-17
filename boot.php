5234e16d2935fef5923c3b46f68f7dffa0c16f07
Alain Kelleter <alain.kelleter@kelleter.be>
2024-09-17 14:02:58 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -6,8 +6,8 @@
     define('DS', DIRECTORY_SEPARATOR);
     
     // Define the installation directory
-    //define('INST', DS.'prj'.DS.'LPB2025'.DS); //work
-    define('INST', DS.'LPB2025'.DS); //home
+    define('INST', DS.'prj'.DS.'LPB2025'.DS); //work
+    //define('INST', DS.'LPB2025'.DS); //home
 
     // Define the root directory
     define('R0', '.'.DS);
