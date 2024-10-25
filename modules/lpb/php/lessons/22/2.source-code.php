f7603bdecc8e2809e345a2600464f5cf77db7fe5
Alain Kelleter <alain.kelleter@kelleter.be>
2024-10-25 15:10:38 +0200

    Update 0.22.0
    Fin de la leçon lpb/php/lessons/22
    Mise en place de la leçon  lbp/php/lessons/23

--- modules/lpb/php/lessons/22/2.source-code.php
+++ modules/lpb/php/lessons/22/2.source-code.php
@@ -4,6 +4,58 @@ session_start();
 require_once($_SESSION['R'].'app/fct.php');
 echo 'PHP VERSION : '.PHP_VERSION.'<br><hr>';
 //----------------------------------------------------------------------------------------------------
-    
-   
-?>
+echol("Exo1 : Créer un tableau multidimensionnel");
+$tab = [
+    'A' => ['a1', 'a2', 'a3'],
+    'B' => ['b1', 'b2', 'b3'],
+    'C' => ['c1', 'c2', 'c3']
+];
+$tab2 = [
+    ['A', 'B', 'C'],
+    ['D', 'E', 'F'],
+    ['G', 'H', 'I']
+];
+$personnages = [
+   "Link" => ["Age" => 17, "Type" => "Epéiste", "Vie" => 3],
+   "Zelda" => ["Age" => 17, "Type" => "Magie", "Vie" => 4],
+   "Ganon" => ["Age" => 25, "Type" => "Tyran", "Vie" => 5]
+];
+DBGPrint($tab, 'PR', '$tab :');
+DBGPrint($tab2, 'PR', '$tab2 :');
+DBGPrint($personnages, 'PR', '$tab3 :');
+hr();
+//----------------------------------------------------------------------------------------------------
+echol("Exo2 : Afficher les valeurs du tableau avec 2 boucles foreach imbriquées");
+foreach($tab as $key => $value) {
+    echo 'Clé : '.$key.'<br>';
+    foreach($value as $v) {
+        echo ' - Value : '.$v.'<br>';
+    }
+}
+echol("-------------------------------");
+foreach($tab2 as $key => $value) {
+    echo 'Clé : '.$key.'<br>';
+    foreach($value as $v) {
+        echo ' - Value : '.$v.'<br>';
+    }
+}
+echol("-------------------------------");
+foreach($personnages as $perso => $properties) {
+    echo 'Personnage : '.$perso.'<br>';
+    foreach($properties as $name => $value) {
+        echo ' - ' . $name . ' : '.$value.'<br>';
+    }
+}
+hr();
+//----------------------------------------------------------------------------------------------------
+echol("Exo3 : Afficher les valeurs d'un tableau avec print_r et var_dump");
+echol("<b>Avec print_r() :</b>");
+echo "<pre>";
+print_r($tab);
+echo "</pre>";
+echol("<br>Avec <b>var_dump :</b>");
+echo "<pre>";
+var_dump($tab);
+echo "</pre>";
+hr();
+//----------------------------------------------------------------------------------------------------
--- modules/lpb/php/lessons/22/4.explanations.php
+++ modules/lpb/php/lessons/22/4.explanations.php
@@ -1,7 +1,7 @@
 <?php
     $explanations = [
-        "",
-        "",
-        "",
-        "",                                                              
+        "Les tableaux multidimensionnels sont des tableaux qui contiennent d'autres tableaux. Ils sont très utiles pour stocker des données de manière structurée.",
+        "On crée un tableau multidimensionnel en définissant des clés qui contiennent des tableaux.",
+        "On utilise des boucles foreach imbriquées pour parcourir les tableaux multidimensionnels.",
+        "On peut afficher les valeurs des tableaux multidimensionnels avec les fonctions print_r() et var_dump()."        
     ];
