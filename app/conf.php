66e7ed6cecb523c29c6b462d88752281448bb45d
Alain Kelleter <alain.kelleter@kelleter.be>
2024-10-30 16:44:59 +0100

    Update 0.29.0
    - Début de la leçon lpb/php/lessons/29

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '29102024 - 1635';
-    const APP_VERSION = 'v0.28.1'.'-'.APP_UPDATE;
+    const APP_UPDATE = '30102024 - 1615';
+    const APP_VERSION = 'v0.29.0'.'-'.APP_UPDATE;
     const APP_YEAR = '2024-2025';
     const APP_AUTHOR = 'A. Kelleter';
     
--- modules/lpb/php/lessons/29/2.source-code.php
+++ modules/lpb/php/lessons/29/2.source-code.php
@@ -4,6 +4,20 @@ session_start();
 require_once($_SESSION['R'].'app/fct.php');
 echo 'PHP VERSION : '.PHP_VERSION.'<br><hr>';
 //----------------------------------------------------------------------------------------------------
-    
-   
-?>
+echol("Exo1 :  Obtenir et afficher le TimeStamp actuel");
+$timestamp = time();
+echo $timestamp;        
+hr();
+//----------------------------------------------------------------------------------------------------
+echol("Exo2 :  Obtenir et afficher le Timestamp d'une date donnée");
+// Avec la fonction strtotime
+$timestamp1 = strtotime("2023-12-31 23:59:59");
+echo 'Avec strtotime() : ' . $timestamp1 . '<br>';
+// Avec la fonction mktime
+$timestamp2 = mktime(23, 59, 59, 12, 31, 2023);
+echo 'Avec mktime() : ' . $timestamp2 . '<br>';
+// Avec la fonction gmmktime
+$timestamp3 = gmmktime(23, 59, 59, 12, 31, 2023);
+echo 'Avec gmmktime() : ' . $timestamp3 . '<br>';
+hr();
+//----------------------------------------------------------------------------------------------------
