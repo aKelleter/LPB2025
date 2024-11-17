56ea5ab4f4df2b6ac5254637b08a515c99120850
Alain Kelleter <alain.kelleter@kelleter.be>
2024-11-17 22:02:22 +0100

    Update 0.29.4
    - Mises à jours mineures et corrections

--- modules/lpb/php/lessons/04/2.source-code.php
+++ modules/lpb/php/lessons/04/2.source-code.php
@@ -1,9 +1,11 @@
 <?php
 echo 'PHP VERSION : '.PHP_VERSION.'<br><hr>'; 
    // Déclaration  et intialisation de nos premières variables
-   // Le signe = est l'opérateur d'affectation !!
+   // A SAVOIR : Le signe '=' est l'opérateur d'affectation !!
 
    // Nous allons enregistrer le nom, le prénom et l'âge d'une personne
+   // On parle de déclaration et d'initialisation de variables
+
     $nom = "Doe";       // Le nom de la personne, ici c'est "Doe" et c'est une chaîne de caractères (string) 
     $prenom = "John";   // Le prénom de la personne, ici c'est "John" et c'est une chaîne de caractères (string)
     $age = 25;          // L'âge de la personne, ici c'est 25 et c'est un nombre entier (integer)   
@@ -13,17 +15,17 @@ echo 'PHP VERSION : '.PHP_VERSION.'<br><hr>';
     echo $prenom, $nom, $age;
     echo '<br>--------------------------------<br>';
 
-    // Réalisons un affichage (avec des guillemets simples) plus lisible avec l'aide de l'opérateur de concaténation (le point .) 
-    echo 'Affichage plus "propre" des variables nom, prenom et age avec la commande echo (et des guillemets simples) et le caractère de concaténation<br>';
-    echo 'Prénom : '.$prenom.', Nom : '.$nom.', Age : '.$age;
-    echo '<br>--------------------------------<br>';
-
-    // Réalisons un affichage (avec des guillemets doubles) plus lisible 
-    echo 'Affichage plus "propre" des variables nom, prenom et age avec la commande echo (et des guillemets doubles)<br>';
+    // Affichons ensuite les variables prenom et nom sans concaténation en utilisant les guillemets doubles 
+    echo 'Affichage avec des guillemets doubles, sans concanténations : <br>';
     echo "Prénom : $prenom, Nom : $nom, Age : $age";
-    echo '<br>--------------------------------<br>';
+    echo '<br><br>';
 
-    // Mettre à jour le contenu d'une variable
+    // Pour finir, affichons les variables prenom et nom avec concaténation (concaténation signifie : collées les unes après les autres)
+    echo 'Affichage avec concaténations avec des guillemets simples : <br>';
+    echo 'Prénom : '.$prenom.', Nom : '.$nom.', Age : '.$age;
+    echo '<br><br>';
+
+    // Comment mettre à jour le contenu d'une variable ?
     echo 'Affichage de la variable prenom avant la mise à jour : '.$prenom.'<br>';
     $prenom = "Jane"; // On met à jour la variable prenom avec la valeur "Jane"
     echo 'Affichage de la variable prenom après mise à jour : '.$prenom;
--- modules/lpb/php/lessons/04/4.explanations.php
+++ modules/lpb/php/lessons/04/4.explanations.php
@@ -4,5 +4,6 @@
         "Elles sont précédées du symbole <code>$</code> suivi du nom de la variable.",
         "Leur nom est sensible à la casse et doit respecter une série de règles.",
         "Les données stockées dans une variable peut varier au fil du temps.", 
-        "Leur type peut être un entier, une chaîne de caractères, un booléen, un tableau, etc.",                              
+        "Leur type peut être un entier, une chaîne de caractères, un booléen, un tableau, etc.",   
+        "Dans le cadre des affichages, on utilise souvent le point <code>.</code> pour concaténer des chaînes de caractères.",                           
     ];
