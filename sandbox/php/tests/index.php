5980d9e08aec38e81c46d139b4e9e71a7fc2f355
Alain Kelleter <alain.kelleter@kelleter.be>
2025-01-10 17:28:04 +0100

    Update index.php

--- sandbox/php/tests/index.php
+++ sandbox/php/tests/index.php
@@ -6,3 +6,29 @@ define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . 
 
 echo BASE_PATH . '<br>';
 echo BASE_URL . '<br>';
+
+// --------------------------------------------------------
+
+// COOKIE
+
+// Création d'un cookie
+//setcookie('test', 'on', time() + 3600, '/', 'localhost', false, true);
+
+
+// Lecture d'un cookie
+if (isset($_COOKIE['test'])) {
+
+    if ($_COOKIE['test'] === 'on') {
+        echo 'Cookie test ACTIF : ' . $_COOKIE['test'] . '<br>';
+        setcookie('test', 'off', time() + 3600, '/', 'localhost', false, true);
+    }elseif($_COOKIE['test'] === 'off') {
+        echo 'Cookie test INACTIF : ' . $_COOKIE['test'] . '<br>';
+        setcookie('test', 'on', time() + 3600, '/', 'localhost', false, true);
+    }else{
+        echo 'Cookie test INCONNU : ' . $_COOKIE['test'] . '<br>';
+    }
+
+}else{
+    echo 'Cookie test NON EXISTANT<br>';
+}
+
