017b5f4eb2017ab9cd0b2e3595aeab14c34f31af
Alain Kelleter <alain.kelleter@kelleter.be>
2025-01-13 14:17:30 +0100

    Update 0.49.1
    Minor update

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '10012025 - 16:15';
-    const APP_VERSION = 'v0.49.0'.' - '.APP_UPDATE;
+    const APP_UPDATE = '13012025 - 16:20';
+    const APP_VERSION = 'v0.49.1'.' - '.APP_UPDATE;
 
     const APP_YEAR = '2024-2025';
     const APP_AUTHOR = 'A. Kelleter';
