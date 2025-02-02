b6ea5d3a330da8a2099d754b23d1fe96779fe29c
Alain Kelleter <alain.kelleter@kelleter.be>
2025-02-02 15:20:01 +0100

    Update 0.59.0
    - Ecriture de la leçon 05 du mod LDB

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application   
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '31012025 - 21:10';
-    const APP_VERSION = 'v0.58.0'.' - '.APP_UPDATE;
+    const APP_UPDATE = '02022025 - 15:20';
+    const APP_VERSION = 'v0.59.0'.' - '.APP_UPDATE;
 
     const APP_YEAR = '2024-2025';
     const APP_AUTHOR = 'A. Kelleter';
