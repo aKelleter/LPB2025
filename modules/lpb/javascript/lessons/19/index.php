6793135bbc903d3892f511d5084427a2976966bb
Alain Kelleter <alain.kelleter@kelleter.be>
2025-02-09 21:56:06 +0100

    Update 0.65.0
     - End lesson 9 LDB
     - Start lesson 10 LDB
     - End lessons 18, 19, 20 LPB/JS

---
+++ modules/lpb/javascript/lessons/19/index.php
@@ -0,0 +1,39 @@
+<?php 
+    require_once('../../../../../boot.php');  
+    $_SESSION['R'] = R5;
+    require_once($_SESSION['R'].'app'.DS.'conf.php'); 
+    $_SESSION['PAGE'] = JS;     
+    
+    // Chargement du/des fichier(s)    
+    $source_code = loadFile(FILENAME_SOURCE_CODE);     
+    require_once(FILENAME_EXPLANATION);
+    require_once(FILENAME_RESSOURCES);
+?>
+<!doctype html>
+<html lang="fr">
+    <?php require_once $_SESSION['R'].'app'.DS.'head.php'; ?>
+    <body>  
+    <?php require_once $_SESSION['R'].'app'.DS.'header.php'; ?>
+        <div class="b-divider"></div>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">                       
+                        <h1 class="mt-5 text-center">Leçon 19 : <span class="color_dark_green">Les Expressions de Fonction</span></h1>  
+                        <?= getHtmlSubMenu('18', '20', JS); ?> 
+                        
+                        <?= getHtmlBlocWhatCodeDoes(file_get_contents(FILENAME_WCD)); ?>
+                      
+                        
+                        <button onclick="scrollToTop()" class="btn btn-primary mb-4" id="scrollToTopBtn">Haut de page</button> 
+                    </div> <!--col-md-10-->
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+        </main>
+        <?php require_once $_SESSION['R'].'app'.DS.'footer.php'; ?>
+        <?php require_once $_SESSION['R'].'app'.DS.'loadscripts.php'; ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
