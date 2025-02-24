d6785f10fa670b56854693b7cd21909c2f357b3a
Alain Kelleter <alain.kelleter@kelleter.be>
2025-02-24 15:26:57 +0100

    Update 0.75.1
     - Todd List (console)

---
+++ modules/lpb/javascript/evals/todo-list/index.php
@@ -0,0 +1,49 @@
+<?php 
+    require_once('../../../../../boot.php');  
+    $_SESSION['R'] = R5;
+    require_once($_SESSION['R'].'app'.DS.'conf.php'); 
+    $_SESSION['PAGE'] = PHP;      
+?>
+<!doctype html>
+<html lang="fr">
+    <?php require_once $_SESSION['R'].'app'.DS.'head.php'; ?>
+    <body>    
+    <?php require_once $_SESSION['R'].'app'.DS.'header.php'; ?>
+        <div class="b-divider"></div>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">                     
+                        <h1 class="mt-5 text-center">Evaluation : <span class="color_dark_green">To-do List</span></h1>
+                        <h3><b>Atelier</b> : Créer une To-do List Interactive en JavaScript</h3>
+
+                        <p><b>Objectif</b> : Mettre en pratique les concepts de base du JavaScript (variables, fonctions, boucles, conditions, objets, etc.) en construisant une application simple de gestion de tâches dans la console ou une page web.</p>
+
+                        <p><b>Prérequis</b></p>
+                        <ul>
+                            <li>Votre éditeur de code/texte.
+                            <li>Une connaissance des notions listées (variables, fonctions, boucles, etc.).</li>
+                            <li>Un navigateur pour tester le code.</li>
+                        </ul>
+                        
+                        <p><b>Introduction</b></p>
+                        
+                        <p>
+                            Vous  aller écrire une application simple de gestion de tâches. Vous pourrez ajouter des tâches, les supprimer et les marquer comme terminées à l'aide de ce que vous 
+                            avez déjà appris en JavaScript.
+                        </p>
+                        <a href="soluce/index.html">Demo</a>
+                        <p>
+                            Happy Coding :)
+                        </p>    
+                        </div> <!--col-md-10-->
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+        </main>
+        <?php require_once $_SESSION['R'].'app'.DS.'footer.php'; ?>
+        <?php require_once $_SESSION['R'].'app'.DS.'loadscripts.php'; ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
