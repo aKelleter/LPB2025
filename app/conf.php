937ccb179bbad801f4bf2ae63f83782b2b27a8bd
Alain Kelleter <alain.kelleter@kelleter.be>
2025-03-15 15:23:57 +0100

    Update 0.82.0
    - Publication des soluces pour LPB/JS : Calculatrice et Master Mind

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application   
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '15032025 - 15:30';
-    const APP_VERSION = 'v0.81.0'.' - '.APP_UPDATE;
+    const APP_UPDATE = '15032025 - 15:20';
+    const APP_VERSION = 'v0.82.0'.' - '.APP_UPDATE;
 
     const APP_YEAR = '2024-2025';
     const APP_AUTHOR = 'A. Kelleter';
