286522bc56cc487faa78fc8c0894f6dad54b6dd3
Alain Kelleter <alain.kelleter@kelleter.be>
2025-03-16 20:59:11 +0100

    Update 0.83.0
    - Add lessons 30, 31, 32 on LPB/JS

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application   
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '15032025 - 15:20';
-    const APP_VERSION = 'v0.82.0'.' - '.APP_UPDATE;
+    const APP_UPDATE = '16032025 - 15:40';
+    const APP_VERSION = 'v0.83.0'.' - '.APP_UPDATE;
 
     const APP_YEAR = '2024-2025';
     const APP_AUTHOR = 'A. Kelleter';
