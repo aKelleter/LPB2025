6ace04672708ab2b5a48d94e50124915c22c307c
Alain Kelleter <alain.kelleter@kelleter.be>
2025-04-04 11:08:19 +0200

    Update 0.92.0
    - Start lesson 42 on LPB/JS

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application   
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '01042025 - 16:45';
-    const APP_VERSION = 'v0.91.0'.' - '.APP_UPDATE;
+    const APP_UPDATE = '04042025 - 16:50';
+    const APP_VERSION = 'v0.92.0'.' - '.APP_UPDATE;
 
     const APP_YEAR = '2024-2025 ';
     const APP_AUTHOR = 'A. Kelleter';
