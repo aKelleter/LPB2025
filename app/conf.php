9a0a4948ded4e83bff20362d1da68c7f09795958
Alain Kelleter <alain.kelleter@kelleter.be>
2025-04-08 16:09:27 +0200

    Update 0.94.0
    - Add new lessons on LDB

--- app/conf.php
+++ app/conf.php
@@ -2,8 +2,8 @@
   
     // Définition des constantes de l'application   
     const APP_NAME = 'LPB - LDB <sup>3</sup>';
-    const APP_UPDATE = '07042025 - 16:40';
-    const APP_VERSION = 'v0.93.0'.' - '.APP_UPDATE;
+    const APP_UPDATE = '08042025 - 16:45';
+    const APP_VERSION = 'v0.94.0'.' - '.APP_UPDATE;
 
     const APP_YEAR = '2024-2025 ';
     const APP_AUTHOR = 'A. Kelleter';
