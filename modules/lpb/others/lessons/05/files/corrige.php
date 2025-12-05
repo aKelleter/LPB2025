798c77bf2d2df49dfd616b9a2798a0da9250afa7
Alain Kelleter <alain.kelleter@kelleter.be>
2025-12-05 15:45:42 +0100

    Update 05122025-1545

---
+++ modules/lpb/others/lessons/05/files/corrige.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Corrigé de l'exercice</title>
+</head>
+<body>
+    
+<h3>1. Reformulation</h3>
+<p>
+    Le programme reçoit l’attaque du héros et la défense du monstre.
+    Il calcule les dégâts en réduisant l’attaque selon la défense, puis affiche le nombre de dégâts finaux.    
+</p>
+<p>Validé si sa réponse mentionne bien :</p>
+
+</body>
+</html>
